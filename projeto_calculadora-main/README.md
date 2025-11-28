bea0894dc1a4c458c8d34dfcb45109d4feabce4a
Jandoza <139405769+Jandoza@users.noreply.github.com>
2025-11-27 21:39:02 -0300

    Add files via upload

---
+++ projeto_calculadora-main/README.md
@@ -0,0 +1,3 @@
+
+
+![alt text](<Captura de tela 2025-11-27 213420.png>)
---
+++ projeto_calculadora-main/calculadora/TesteCalculadora.java
@@ -0,0 +1,23 @@
+package calculadora;
+
+public class TesteCalculadora {
+
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+
+        // testes 
+        calc.calc(2, 3, "+");   // esperado: 5
+        
+        calc.calc(10, 4, "-");  // esperado: 6
+        
+        calc.calc(3, 5, "*");   // esperado: 15
+
+        calc.calc(8, 2, "/");   // esperado: 4
+
+        calc.calc(8, 0, "/");   // divisão porzero
+
+        calc.calc(5, 5, "x");   // operação inválida
+    }
+}
+
+
